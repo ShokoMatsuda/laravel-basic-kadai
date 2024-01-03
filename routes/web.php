78a59438c7fd72c7cf13707b0c6246bf41fe33c0
ShokoMatsuda <syokom.0128@gmail.com>
2024-01-03 17:04:31 +0900

    静的なページを表示しよう

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/posts', [PostController::class, 'index']);
