6f8a0539bebf75c340473c0f0fa4478e081da6c3
ShokoMatsuda <syokom.0128@gmail.com>
2024-01-20 19:16:06 +0900

    課題データの作成機能とバリデーションを実装しよう実装中

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,7 +36,7 @@ class PostController extends Controller
         ]);
 
         //フォームの入力内容をもとに、テーブルにデータを追加する
-        $posts = new Posts();
+        $posts = new Post();
         $posts->title = $request->input('title');
         $posts->content = $request->input('content');
         $posts->save();
