e451f17a7987a2c7a7efa2094300360f21101950
ShokoMatsuda <syokom.0128@gmail.com>
2024-01-20 13:40:44 +0900

    課題データの作成機能とバリデーションを実装しよう

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\PostController;
 
 /*
@@ -22,3 +21,7 @@ Route::get('/', function () {
 route::get('/posts', [PostController::class, 'index']);
 
 Route::get('posts/{id}', [PostController::class, 'show']);
+
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::post('/posts/store', [PostController::class, 'store']);
